663086167b399f890e2ef6895ae115e56fc9c34a
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-19 18:58:54 -0300

    initialized project

---
+++ src/pages/home-page/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { HelloWorld } from "./styles";
+
+const HomePage = () => {
+  return (
+    <div>
+      <HelloWorld>Hello, World!!!</HelloWorld>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/home-page/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const HelloWorld = styled.h1`
+    color: tomato;
+    font-size: 24px;
+`;
