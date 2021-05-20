3ac4e664cc989ace46a2d9987ef9c2db105a3812
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-20 11:50:10 -0300

    finished adding initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 
-import { Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/home-page";
+import LoginPage from "./pages/log-in-page";
+import SignInPage from "./pages/sign-in-page";
+import MapPage from "./pages/map-page";
 
 import "./App.css";
 
 function App() {
   return (
     <Switch>
-      <HomePage />
+      <Route path="/" exact component={HomePage} />
+      <Route path="/log-in" exact component={LoginPage} />
+      <Route path="/sign-in" exact component={SignInPage} />
+      <Route path="/map" component={MapPage} />
     </Switch>
   );
 }
