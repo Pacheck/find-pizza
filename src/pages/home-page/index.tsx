980962954ba9127954900d7fa98204f1ddc35bb8
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-22 23:19:42 -0300

    added home page

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,23 +1,11 @@
 import React from "react";
-
-import { Button, Input } from "semantic-ui-react";
-
-import { signInWithGoogle } from "../../firebase/utils";
+import { useSelector } from "react-redux";
 
 const HomePage = () => {
-  return (
-    <div>
-      <Button onClick={signInWithGoogle}>Go to log-in</Button>
-      <Input
-        action={{
-          icon: "search",
-          onClick: () => console.log("An action was clicked!"),
-        }}
-        actionPosition="left"
-        placeholder="Search..."
-      />
-    </div>
-  );
+  const user = useSelector((state: any) => state.user.currentUser);
+  console.log(user);
+
+  return <div>Home page</div>;
 };
 
 export default HomePage;
--- src/pages/map-page/index.tsx
+++ src/pages/map-page/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 
+import { useHistory } from "react-router-dom";
+
+import { auth } from "../../firebase/utils";
+
 const MapPage = () => {
+  const history = useHistory();
+
   return (
     <div>
       <h1>Testando map!</h1>
+      <button
+        onClick={() => {
+          auth.signOut();
+          history.push("/user");
+        }}
+      >
+        Deslogar
+      </button>
     </div>
   );
 };
