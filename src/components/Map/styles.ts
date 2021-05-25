9a25a2c1415ac10b9006eca098dda06a52fae597
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-25 03:13:37 -0300

    created map component with leaflet

---
+++ src/components/Map/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+import { MapContainer } from 'react-leaflet';
+
+export const CustomizedMap = styled(MapContainer)`
+    width: 100vw;
+    height: 100vh;
+`;
+
